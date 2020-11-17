77c8778af38afeb406809305310f63d0bf87133d
Tony Wang <zichutonywang@gmail.com>
2020-11-17 11:49:13 -0500

    Photo(s) picker button appears to be working as well.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -27,7 +28,7 @@ namespace UWP_Button_to_execute_code
             this.InitializeComponent();
         }
 
-        private void Photo_Click(object sender, RoutedEventArgs e)
+        private async void Photo_Click(object sender, RoutedEventArgs e)
         {
             var picker = new Windows.Storage.Pickers.FileOpenPicker();
             picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
@@ -40,17 +41,44 @@ namespace UWP_Button_to_execute_code
             if (file != null)
             {
                 // Application now has read/write access to the picked file
-                this.textBox.Text = "Picked photo: " + file.Name;
+                this.textBoxPhoto.Text = "Picked photo: " + file.Name;
             }
             else
             {
-                this.textBox.Text = "Operation cancelled.";
+                this.textBoxPhoto.Text = "Operation cancelled.";
             }
         }
 
         private void FileExplorer_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private async void Photos_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".jpg");
+            picker.FileTypeFilter.Add(".jpeg");
+            picker.FileTypeFilter.Add(".png");
+
+            var files = await picker.PickMultipleFilesAsync();
+            if (files.Count > 0)
+            {
+                StringBuilder output = new StringBuilder("Picked files:\n");
+
+                // Application now has read/write access to the picked file(s)
+                foreach (Windows.Storage.StorageFile file in files)
+                {
+                    output.Append(file.Name + "\n");
+                }
+                this.textBoxPhotos.Text = output.ToString();
+            }
+            else
+            {
+                this.textBoxPhotos.Text = "Operation cancelled.";
+            }
         }
     }
 }
